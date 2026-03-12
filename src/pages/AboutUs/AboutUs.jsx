16309733cf37c99a4800a0d60b6de8fc1f4b224c
AhmedTyson <AhmedTyson@users.noreply.github.com>
2026-03-12 23:50:49 +0200

    feat: Introduce About Us page and add various website image assets.

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,5 +1,7 @@
 import './AboutUs.css';
 function AboutUs() {
-  return <div className="min-h-screen py-24 container"><h1 className="text-4xl font-bold">About Us Page</h1></div>;
+  return <div className="min-h-screen py-24 container"><h1 className="text-4xl font-bold">About Us Page</h1>
+  <div></div>
+  </div>;
 }
 export default AboutUs;
